223b7691ecf74dd962fc937dcfd50b14513299ad
Shridhish R S <Shridhish7@gmail.com>
2017-11-15 05:26:04 -0500

    Update Recursion_Count8.java
    http://codingbat.com/prob/p192383

--- Recursion_Count8.java
+++ Recursion_Count8.java
@@ -1,5 +1,9 @@
 public int count8(int n) {
   
+  /* Given a non-negative int n, compute recursively (no loops) the count of 
+ * the occurrences of 8 as a digit, except that an 8 with another 8 
+ * immediately to its left counts double, so 8818 yields 4.
+ */
   if(n==0)
     return 0;
     
