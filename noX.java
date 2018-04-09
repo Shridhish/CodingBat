b7e2c1107572e84825209c3fbd200921d808976d
Shridhish R S <Shridhish7@gmail.com>
2018-04-09 01:09:00 -0400

    Create noX.java

---
+++ noX.java
@@ -0,0 +1,14 @@
+public String noX(String str) {
+  if(str.length() == 0) return "";
+  
+  
+  if(str.charAt(str.length()-1) == 'x' ) {
+    return noX(str.substring(0,str.length()-1))+"";
+  }
+  
+  
+  return noX(str.substring(0,str.length()-1))+str.charAt(str.length()-1);
+  
+  
+  
+}
