fd0730bc5c2643823f99d3cc1e6abcaeafdaab4b
Shridhish R S <Shridhish7@gmail.com>
2018-04-08 23:32:40 -0400

    Update Recursion_ChangeXY.java

--- Recursion_ChangeXY.java
+++ Recursion_ChangeXY.java
@@ -20,3 +20,18 @@ if(str.charAt(0)=='x') {
 
 return str.charAt(0)+changeXY(str.substring(1));
 }
+
+/*  Bottom up
+public String changeXY(String str) {
+  
+  if(str.length() == 0) return "";
+  
+  if(str.charAt(str.length()-1) == 'x') {
+    return changeXY(str.substring(0,str.length()-1))+"y";
+  }
+  
+  return changeXY(str.substring(0,str.length()-1))+str.charAt(str.length()-1);
+  
+  
+  
+} */
